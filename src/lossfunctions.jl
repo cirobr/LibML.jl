f162f924a951c1e1c1c12a8d61c6c69e1bde8ad5
cirobr <ciro.rosa@alumni.usp.br>
2023-12-22 04:36:35 +0000

    new code

--- src/lossfunctions.jl
+++ src/lossfunctions.jl
@@ -1,9 +1,12 @@
 function IoU(yhat::AbstractArray, y::AbstractArray)
     yc = clamp.(yhat, 0.0, 1.0)
 
-    intersection = sum(yc .* y)
-    union        = sum(yc .+ y) - intersection
-    iou = intersection / (union + eps(Float32))
+    # intersection = sum(yc .* y)
+    # union        = sum(yc .+ y) - intersection
+    # iou = intersection / (union + eps(Float32))
+    i = sum(yc .* y)
+    u = sum(yc .+ y .- i)
+    iou = i / (u + eps(Float32))
 
     return iou |> Float32
 end
@@ -12,4 +15,5 @@ IoU_loss(yhat::AbstractArray, y::AbstractArray) = 1.0f0 - IoU(yhat, y)
 
 
 tv07_loss(yhat::AbstractArray, y::AbstractArray) = Flux.tversky_loss(yhat, y, beta=0.7)
+
 tv03_loss(yhat::AbstractArray, y::AbstractArray) = Flux.tversky_loss(yhat, y, beta=0.3)
