ba4d8124a43c6ed5098247e1427e9cb66d8352eb
cirobr <ciro.rosa@alumni.usp.br>
2023-12-28 19:54:22 -0300

    removed custom tversky loss

--- src/lossfunctions.jl
+++ src/lossfunctions.jl
@@ -6,8 +6,3 @@ function IoU(yhat::AbstractArray, y::AbstractArray)
 end
 
 IoU_loss(yhat::AbstractArray, y::AbstractArray) = 1.0 - IoU(yhat, y) |> Float32
-
-
-tv07_loss(yhat::AbstractArray, y::AbstractArray) = Flux.tversky_loss(yhat, y, beta=0.7) |> Float32
-
-tv03_loss(yhat::AbstractArray, y::AbstractArray) = Flux.tversky_loss(yhat, y, beta=0.3) |> Float32
