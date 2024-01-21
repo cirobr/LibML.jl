a504672ea27d0ed9c43688f1999f41b9e2b7b444
cirobr <ciro.rosa@alumni.usp.br>
2024-01-21 15:17:57 -0300

    commit trainModel!()

---
+++ src/training.jl
@@ -0,0 +1,33 @@
+function trainModel!(model, data, optstate, lossfn; verbose=false)
+    steplosses = Vector{Float32}(undef, length(data))
+
+    for (i, (X,y)) in enumerate(data)
+        loss, grads = Flux.withgradient(model) do m
+            lossfn(m(X), y)
+        end
+
+        Flux.update!(optstate, model, grads[1])
+        steplosses[i] = loss
+    end
+
+    epochloss = mean(steplosses)
+
+    if verbose   return epochloss, steplosses
+    else         return epochloss
+    end
+end
+
+
+# # lossfns is a vector of loss functions
+# function testModel(model, data, lossfns)
+#     losses = Array{Float32,2}(undef, (length(data), length(lossfns)))
+
+#     for (i, (X,y)) in enumerate(data)
+#         yhat = model(X)
+#         for (j, lossfn) in enumerate(lossfns)
+#             losses[i,j] = lossfn(yhat, y)
+#         end
+#     end
+
+#     return losses
+# end
