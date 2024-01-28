5e69390e7a835bd74737a28188dc9d0028d97528
cirobr <ciro.rosa@alumni.usp.br>
2024-01-28 09:01:23 -0300

    bug fix completed

--- src/scores.jl
+++ src/scores.jl
@@ -1,4 +1,4 @@
-function IoUScore(yhat::Array{Float32}, y::Array{Bool}; threshold=0.5)
+function IoUScore(yhat::AbstractArray, y::AbstractArray; threshold=0.5)
     yth = map(x -> x > threshold ? true : false, yhat)
     return IoU(yth, y) |> Float32
 end
@@ -19,7 +19,7 @@ function F1Score(yhat::AbstractArray, y::AbstractArray; threshold=0.5)
 end
 
 
-function PrecisionScore(yhat::Array{Float32}, y::Array{Bool}; threshold=0.5)
+function PrecisionScore(yhat::AbstractArray, y::AbstractArray; threshold=0.5)
     yth = map(x -> x > threshold ? true : false, yhat)
     cm = sm.ConfusionMatrix(levels=Bool[0,1])(yth, y)
     return sm.positive_predictive_value(cm) |> Float32
@@ -27,7 +27,7 @@ end
 const PPVScore = PrecisionScore
 
 
-function RecallScore(yhat::Array{Float32}, y::Array{Bool}; threshold=0.5)
+function RecallScore(yhat::AbstractArray, y::AbstractArray; threshold=0.5)
     yth = map(x -> x > threshold ? true : false, yhat)
     cm = sm.ConfusionMatrix(levels=Bool[0,1])(yth, y)
     return sm.true_positive_rate(cm) |> Float32
@@ -35,14 +35,14 @@ end
 const TPRScore = RecallScore
 
 
-function FPRScore(yhat::Array{Float32}, y::Array{Bool}; threshold=0.5)
+function FPRScore(yhat::AbstractArray, y::AbstractArray; threshold=0.5)
     yth = map(x -> x > threshold ? true : false, yhat)
     cm = sm.ConfusionMatrix(levels=Bool[0,1])(yth, y)
     return sm.false_positive_rate(cm) |> Float32
 end
 
 
-function TNRScore(yhat::Array{Float32}, y::Array{Bool}; threshold=0.5)
+function TNRScore(yhat::AbstractArray, y::AbstractArray; threshold=0.5)
     yth = map(x -> x > threshold ? true : false, yhat)
     cm = sm.ConfusionMatrix(levels=Bool[0,1])(yth, y)
     return sm.true_negative_rate(cm) |> Float32
