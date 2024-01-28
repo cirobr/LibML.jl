c9f76aeab569b504b5af9fb7b9fd3e7ea5a029b7
cirobr <ciro.rosa@alumni.usp.br>
2024-01-27 23:12:12 -0300

    replace dot operator by map()

--- src/scores.jl
+++ src/scores.jl
@@ -1,49 +1,56 @@
 function IoUScore(yhat, y; threshold=0.5)
-    yt = yhat .> threshold
-    return IoU(yt, y) |> Float32
+    # yth = yhat .> threshold
+    yth = map(x -> x > threshold ? 1 : 0, yhat)
+    return IoU(yth, y) |> Float32
 end
 
 
 function AccScore(yhat, y; threshold=0.5)
-    yt = yhat .> threshold
-    cm = sm.ConfusionMatrix(levels=Bool[0,1])(yt, y)
+    # yth = yhat .> threshold
+    yth = map(x -> x > threshold ? 1 : 0, yhat)
+    cm = sm.ConfusionMatrix(levels=Bool[0,1])(yth, y)
     return sm.accuracy(cm) |> Float32
 end
 const AccuracyScore = AccScore
 
 
 function F1Score(yhat, y; threshold=0.5)
-    yt = yhat .> threshold
-    cm = sm.ConfusionMatrix(levels=Bool[0,1])(yt, y)
+    # yth = yhat .> threshold
+    yth = map(x -> x > threshold ? 1 : 0, yhat)
+    cm = sm.ConfusionMatrix(levels=Bool[0,1])(yth, y)
     return sm.Functions.fscore(cm) |> Float32
 end
 
 
 function PrecisionScore(yhat, y; threshold=0.5)
-    yt = yhat .> threshold
-    cm = sm.ConfusionMatrix(levels=Bool[0,1])(yt, y)
+    # yth = yhat .> threshold
+    yth = map(x -> x > threshold ? 1 : 0, yhat)
+    cm = sm.ConfusionMatrix(levels=Bool[0,1])(yth, y)
     return sm.positive_predictive_value(cm) |> Float32
 end
 const PPVScore = PrecisionScore
 
 
 function RecallScore(yhat, y; threshold=0.5)
-    yt = yhat .> threshold
-    cm = sm.ConfusionMatrix(levels=Bool[0,1])(yt, y)
+    # yth = yhat .> threshold
+    yth = map(x -> x > threshold ? 1 : 0, yhat)
+    cm = sm.ConfusionMatrix(levels=Bool[0,1])(yth, y)
     return sm.true_positive_rate(cm) |> Float32
 end
 const TPRScore = RecallScore
 
 
 function FPRScore(yhat, y; threshold=0.5)
-    yt = yhat .> threshold
-    cm = sm.ConfusionMatrix(levels=Bool[0,1])(yt, y)
+    # yth = yhat .> threshold
+    yth = map(x -> x > threshold ? 1 : 0, yhat)
+    cm = sm.ConfusionMatrix(levels=Bool[0,1])(yth, y)
     return sm.false_positive_rate(cm) |> Float32
 end
 
 
 function TNRScore(yhat, y; threshold=0.5)
-    yt = yhat .> threshold
-    cm = sm.ConfusionMatrix(levels=Bool[0,1])(yt, y)
+    # yth = yhat .> threshold
+    yth = map(x -> x > threshold ? 1 : 0, yhat)
+    cm = sm.ConfusionMatrix(levels=Bool[0,1])(yth, y)
     return sm.true_negative_rate(cm) |> Float32
 end
