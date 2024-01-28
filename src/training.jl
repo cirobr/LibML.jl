8c9e95136aa370f6d07579927701a926a7be4fa4
cirobr <ciro.rosa@alumni.usp.br>
2024-01-28 10:17:41 -0300

    wip

--- src/training.jl
+++ src/training.jl
@@ -30,9 +30,9 @@ function testModel(model, data, lossfn, metrics)
 
     for (i, (X,y)) in pb.ProgressBar( enumerate(data) )
         yhat = model(X)
-        metrics[1]#(yhat, y)
-        println(typeof(yhat))
-        println(typeof(y))
+        gpu(metrics[1])   #(yhat, y)
+        # println(typeof(yhat))
+        # println(typeof(y))
         for (j, lfn) in enumerate(lossfns)
             # losses[i,j] = lfn(yhat, y)
         end
